66aeefa380e3dba444ec03167b494bf218090ba0
王梓樯 <qq960826@gmail.com>
2015-08-22 01:15:32 +0800

    Update get the list.py

--- python/Webcrawler-teamtreehouse/get the list.py
+++ python/Webcrawler-teamtreehouse/get the list.py
@@ -4,14 +4,8 @@ import re
 from bs4 import BeautifulSoup
 
 s=requests.session()
-proxies={"http":"http://qq960826@gmail.com:960826wang@hk1.nhpass.com:110"}
+proxies={"http":"http://user:password@hk1.nhpass.com:110"}
 html=s.get("http://teamtreehouse.com/library/type:course",proxies=proxies).text
-#print(html)
-#soup=BeautifulSoup(html,"html5lib")
-#tag=soup.b
-#a=soup.find_all(name="li",attrs={"class":re.compile(r'(card course syllabus [\s\S]*? )" data-activity="syllabus')})
-#a=soup.find_all(name="li",attrs={"class":"card course syllabus topic-ruby "})
-#print(len(a))
 
 match_all=re.compile(r'<li class="card course syllabus [\s\S]*?<a class="title" href="/library/(.*)">[\s\S]*?<h3>(.*?)</h3>')
 match_track_index=re.compile(r'<a class="button secondary" href="(.*)">Explore</a>')
@@ -20,12 +14,12 @@ array_all=[]
 array_track=[]
 result_all=match_all.findall(html)
 
-for i in range(len(result_all)):
+for i in range(len(result_all)):#获取所有课程列表
     array_all.append(result_all[i][0])
 
 html=s.get("http://teamtreehouse.com/tracks",proxies=proxies).text
 result_track_index=match_track_index.findall(html)
-for i in range (len(result_track_index)):
+for i in range (len(result_track_index)):#获取系列课程列表
     url_track_index="http://teamtreehouse.com"+result_track_index[i]
     print(url_track_index)
     html=s.get(url_track_index,proxies=proxies).text
@@ -36,7 +30,7 @@ for i in range (len(result_track_index)):
 
 print(array_track)
 
-for i in range (len(array_track)):
+for i in range (len(array_track)):#排除
     print(i)
     for j in range (len(array_all)):
         if(array_track[i]==array_all[j]):
